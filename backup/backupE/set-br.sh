8e2d01bd16d9d866c3f7673b7496f69e93f31d40
arismaramar <arismar.amar@gmail.com>
2023-12-24 05:17:22 +0700

    v3

---
+++ backup/backupE/set-br.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+
+apt install rclone -y
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/backup/backupE/rclone.conf"
+git clone  https://github.com/magnific0/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default
+host smtp.gmail.com
+port 587
+auth on
+user bckupvpns@gmail.com
+from bckupvpns@gmail.com
+password Yangbaru1 
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+wget -O autobackup "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/backup/backupE/autobackup.sh"
+wget -O backupe "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/backup/backupE/backupe.sh"
+wget -O restore "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/backup/backupE/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/backup/backupE/strt.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/backup/backupE/limitspeed.sh"
+chmod +x autobackup
+chmod +x backupe
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
+cd
+rm -f /root/set-br.sh
---
+++ menu/m-system.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/hokagevpn/theme/color.conf)
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m" 
+export COLOR1="$(cat /etc/hokagevpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+export COLBG1="$(cat /etc/hokagevpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
+###########- END COLOR CODE -##########
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+echo "Checking VPS"
+clear
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│${NC} ${COLBG1}             • SYSTEM MENU •                   ${NC} $COLOR1│$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e ""
+echo -e "${COLOR1} [01]${NC} Panel Domain          $NC" 
+echo -e "${COLOR1} [02]${NC} Speedtest VPS         $NC" 
+echo -e "${COLOR1} [03]${NC} Set Auto Reboot       $NC"
+echo -e "${COLOR1} [04]${NC} Restart All Service   $NC"
+echo -e "${COLOR1} [05]${NC} Cek Bandwith          $NC"
+echo -e "${COLOR1} [06]${NC} Install TCP BBR       $NC"
+echo -e "${COLOR1} [07]${NC} DNS CHANGER           $NC"
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo -e   ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; m-domain ; exit ;;
+2) clear ; speedtest ; exit ;;
+3) clear ; auto-reboot ; exit ;;
+4) clear ; restart ; exit ;;
+5) clear ; bw ; exit ;;
+6) clear ; m-tcp ; exit ;;
+7) clear ; m-dns ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
+esac
